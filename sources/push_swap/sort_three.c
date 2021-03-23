0f3923f251c8ea021eecda073261821ef65ecf6c
lmalki-h <lmalki-h@student.42.fr>
2021-03-23 16:30:57 +0100

    sort sub 6 stacks

--- sources/push_swap/sort_three.c
+++ sources/push_swap/sort_three.c
@@ -5,18 +5,24 @@ void	sort_three(t_stack **a)
 	int		index_min;
 	int		index_max;
 
-	index_max = get_index(*a, get_max(*a));
-	index_min = get_index(*a, get_min(*a));
-	if (index_max == 0 && index_min == 1)
+	while (!is_increasing(*a))
 	{
-		rotate(a);
-	}
-	else if (index_max == 1 && index_min == 2)
-	{
-		reverse_rotate(a);
-	}
-	else
-	{
-		swap(a);
+		index_max = get_index(*a, get_max(*a));
+		index_min = get_index(*a, get_min(*a));
+		if (index_max == 0 && index_min == 1)
+		{
+			rotate(a);
+			printf("ra\n");
+		}
+		else if (index_max == 1 && index_min == 2)
+		{
+			reverse_rotate(a);
+			printf("rra\n");
+		}
+		else
+		{
+			swap(a);
+			printf("sa\n");
+		}
 	}
 }
