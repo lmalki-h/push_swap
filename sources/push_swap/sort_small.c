ae263ed5239761438f130989017eab237d642a05
lmalki-h <lmalki-h@student.42.fr>
2021-03-23 21:13:13 +0100

    removed unused var

--- sources/push_swap/sort_small.c
+++ sources/push_swap/sort_small.c
@@ -6,7 +6,7 @@
 /*   By: lmalki-h <lmalki-h@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/23 21:01:38 by lmalki-h          #+#    #+#             */
-/*   Updated: 2021/03/23 21:01:48 by lmalki-h         ###   ########.fr       */
+/*   Updated: 2021/03/23 21:11:49 by lmalki-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,9 @@
 
 void		sort_small(t_stack **a, size_t size)
 {
-	int		top;
-	int		pivot;
 	t_stack *b;
+	int		pivot;
 
-	top = 0;
 	b = NULL;
 	pivot = get_pivot(*a, size);
 	while (get_size(*a) > 3)
@@ -34,6 +32,7 @@ void		sort_small(t_stack **a, size_t size)
 			printf("ra\n");
 		}
 	}
-	sort_three(a);
+	if (get_size(*a) < 4)
+		sort_three(a);
 	empty_b(&b, a);
 }
